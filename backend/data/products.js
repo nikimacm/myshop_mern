e410767918c50b4e690b696ca40e891c8f6efd90
nikimacm <nickymacm@gmail.com>
2023-03-23 13:27:24 +0000

    added package.json to backend, installed express and created first route in server.js

---
+++ backend/data/products.js
@@ -0,0 +1,104 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Premium Pillow Bird Pattern Beige',
+        image: '/images/all-over-print-premium-pillow-22x22-back-lifestyle-3-6408572ba14c9.png',
+        description: 
+                    'This premium pillow is the perfect decorative accent that will highlight' +
+                    'your designs and complement the room.' + 
+                    'Its made from polyester that has a high-end linen feel to it and will serve you' + 
+                    'as a quality interior element',
+        brand: 'ninjahouse.creations',
+        category: 'Pillows and Blankets',
+        price: '18.99',
+        countInStock: '5',
+        rating: '5',
+        numReviews: '4',
+    },
+
+    {
+        _id: '2',
+        name: 'Premium Pillow Swan Design',
+        image: '/images/all-over-print-premium-pillow-22x22-front-lifestyle-4-6408556265aa2.png',
+        description: 
+                    'This premium pillow is the perfect decorative accent that will highlight your designs and complement the room.' +
+                    'It/s made from polyester that has a high-end linen feel to it and will serve you' + 
+                    'as a quality interior element.' 
+                    ,
+        brand: 'ninjahouse.creations',
+        category: 'Pillows and Blankets',
+        price: '18.99',
+        countInStock: '5',
+        rating: '4.5',
+        numReviews: '8',
+    },
+
+    {
+        _id: '3',
+        name: 'Cork back Coaster flower design',
+        image: '/images/cork-back-coaster-front-64078a51efc3c.png',
+        description: 
+                    'Hello beverage coaster, goodbye mug stain! Introducing the long-awaited Cork-Back Coaster' + 
+                    'that/s designed to protect your countertops and furniture from spills and scratches. It/s crafted' +
+                    'from genuine cork, hardboard, with a high-gloss coating that ensures a quality print. The coaster is' +
+                    'heat-resistant, water-repellent, and easy to clean—just wipe it with a damp cloth. ',
+        brand: 'ninjahouse.creations',
+        category: 'Drinkware and Coasters',
+        price: '4.50',
+        countInStock: '15',
+        rating: '3.5',
+        numReviews: '6',
+    },
+
+    {
+        _id: '4',
+        name: 'Stainless Steel Water Bottle Flower Pattern',
+        image: '/images/stainless-steel-water-bottle-white-17oz-back-640793b369f02.png',
+        description: 
+                    'Say hello to our Stainless Steel Water Bottle made from high-grade stainless steel.' +
+                    'Its double-wall construction makes it perfect for storing both hot and cold liquids.' +
+                    'What/s better than taking that first sip of a coffee in the morning, or having a swig of' +
+                    'icy-cold water after a workout? Enjoying your drinks from an easy-to-carry, beautifully designed,' +
+                    'reusable bottle!',
+        brand: 'ninjahouse.creations',
+        category: 'Drinkware and Coasters',
+        price: '15.00',
+        countInStock: '15',
+        rating: '5',
+        numReviews: '8',
+    },
+
+    {
+        _id: '5',
+        name: 'Throw Blanket- Bird Pattern Beige ',
+        image: '/images/throw-blanket-60x80-lifestyle-640797087a5d6.png',
+        description: 
+                    'A throw blanket can be a striking design element, a dear memento, or simply an amazing accessory' +
+                    'for refreshing afternoon naps. That/s why this custom-printed Throw Blanket is the perfect canvas for your wildest designs.' +
+                    'The soft silk touch fabric yields prints in eye-popping colors and the blanket is warm and cozy.',
+        brand: 'ninjahouse.creations',
+        category: 'Pillows and Blankets',
+        price: '25.00',
+        countInStock: '15',
+        rating: '5',
+        numReviews: '5',
+    },
+
+    {
+        _id: '6',
+        name: 'Throw Blanket - Swan Pattern',
+        image: '/images/throw-blanket-60x80-lifestyle-640856240e13c.png',
+        description: 
+                    'A throw blanket can be a striking design element, a dear memento, or simply an amazing accessory' +
+                    'for refreshing afternoon naps. That/s why this custom-printed Throw Blanket is the perfect canvas for your wildest designs.' +
+                    'The soft silk touch fabric yields prints in eye-popping colors and the blanket is warm and cozy.',
+        brand: 'ninjahouse.creations',
+        category: 'Pillows and Blankets',
+        price: '25.00',
+        countInStock: '15',
+        rating: '5',
+        numReviews: '6',
+    },
+]
+
+export default products
