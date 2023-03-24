f5c24b6546c66147bf01366bfc064f5183b8339d
nikimacm <nickymacm@gmail.com>
2023-03-24 13:01:39 +0000

    downloaded axios and brought in state and functional components from react to fetch from backend.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,14 @@
-import React from 'react'
-import products from '../products'
+import React, { useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    console.log('hello')
+  })
+  
   return (
     <>
         <h1>Latest Products</h1>
