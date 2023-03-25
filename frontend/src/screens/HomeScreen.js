05b8613cb9b4b28ad5db48787ad58a019b371320
nikimacm <nickymacm@gmail.com>
2023-03-25 16:17:31 +0000

    added useEffect to homescreen to fetch products from the backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import axios from 'axios'
 
 const HomeScreen = () => {
   const [products, setProducts] = useState([])
 
   useEffect(() => {
-    console.log('hello')
-  })
+    const fetchProducts = async () => {
+      const { data } = await axios.get ('/api/products')
+      setProducts (data)
+    }
+    fetchProducts()
+  },[])
   
   return (
     <>
